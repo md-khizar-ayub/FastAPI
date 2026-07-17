6f7c23b916c227e4c9298d60761e46184ceaa852
khizaR <mohammedkhiar501@gmail.com>
2026-07-17 09:21:33 +0530

    added patient data and new get method

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
     return {
-        'message' : 'Hellow World (^_^)' 
+        'message' : 'Patient Management System API' 
     }
 
 @app.get("/about")
 def about():
     return {
         'message' : 'I have started learning FastAPI'
+    }
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id : str = Path(..., description='Patient Id', example='P001')):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {
+        'error' : 'patient not found'
     }
